a22db1de89d76e67452000f1e332a096531b6f3e
JuliaVlasova <juliavlasova10111989@gmail.com>
2024-11-03 14:37:11 +0300

    js continue

--- src/js/offer-button.js
+++ src/js/offer-button.js
@@ -1,5 +1,16 @@
 "use strict";
 
-window.onload = function() {
-  
-}
+addEventListener("DOMContentLoaded", (event) => {
+  const offerItems = document.querySelectorAll(".offer-info__item");
+  const offerButton = document.querySelector('.offer-button');
+
+  offerItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      const hrefValue = item.getAttribute('data-href');
+      offerButton.setAttribute('href', hrefValue);
+      offerItems.forEach((i) => i.classList.remove("offer-info__item_active"));
+      this.classList.add("offer-info__item_active");
+    });
+  });
+});
+
